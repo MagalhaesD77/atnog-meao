b67421f8b6f864588f9032cd632d5cdbe03ad50d
Pedro Pereira <pedrocjdpereira@ua.pt>
2024-07-04 17:26:53 +0100

    almost finished, collecting node usage

--- oss/utils/threads/container_info_thread.py
+++ oss/utils/threads/container_info_thread.py
@@ -27,19 +27,19 @@ def get_containers_info():
         try:
             response = requests.get("http://meao-migration:8000/containerInfo")
             idsMonitored = []
-            for container in response.json()["ContainerInfo"]:
-                idsMonitored.append(container["id"])
+            for containerName, container in (response.json()["ContainerInfo"]).items():
+                idsMonitored.append(containerName)
                 node_specs = requests.get(
                     "http://meao-migration:8000/nodeSpecs/" + container["node"]
                 ).json()
-                if container["id"] not in containers:
-                    containers[container["id"]] = {
+                if containerName not in containers:
+                    containers[containerName] = {
                         "ns": container["ns_id"],
                         "node": container["node"],
                         "node_specs": node_specs["NodeSpecs"],
                     }
-                    containers[container["id"]]["node_specs"]["prev_cpu"] = 0
-                    containers[container["id"]]["node_specs"]["prev_timestamp"] = 0
+                    containers[containerName]["node_specs"]["prev_cpu"] = 0
+                    containers[containerName]["node_specs"]["prev_timestamp"] = 0
             idsToDelete = []
             for container_id in containers.keys():
                 if container_id not in idsMonitored:
