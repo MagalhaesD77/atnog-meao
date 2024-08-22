7fd111563c499bcc298f37f539652926174157a8
Pedro Pereira <pedrocjdpereira@ua.pt>
2024-08-22 14:23:30 +0100

    small bug fixing

--- oss/utils/threads/websocket_service_thread.py
+++ oss/utils/threads/websocket_service_thread.py
@@ -3,6 +3,7 @@ import json
 import os
 import queue
 import threading
+import time
 
 import websockets
 from cherrypy.process import plugins
@@ -27,43 +28,38 @@ class WebSocketServiceThread(plugins.SimplePlugin):
         self.t.start()
 
     def run_server(self):
-        try:
-            asyncio.run(send_metrics())
-        except Exception as e:
-            print(f"Could not start websockets server: {e}")
-            pass
+        while True:
+            try:
+                asyncio.run(send_metrics())
+            except Exception as e:
+                print(f"Could not start WebSocket server: {e}")
+                time.sleep(5)  # delay before attempting to restart
 
 
 async def send_metrics():
-    async with websockets.serve(
-        receive_connection, "0.0.0.0", os.getenv("OSS_WS_PORT")
-    ):
-        try:
-            while True:
+    async with websockets.serve(receive_connection, "0.0.0.0", os.getenv("OSS_WS_PORT")):
+        while True:
+            try:
                 if not metrics_queue.empty():
-                    metrics = metrics_queue.get()
+                    metrics = metrics_queue.get_nowait()
                     for websocket in ws:
                         await websocket.send(json.dumps(metrics))
                 if not lat_queue.empty():
                     lat = lat_queue.get()
-                    print("sending data")
-                    print(lat)
                     for websocket in ws:
                         await websocket.send(json.dumps(lat))
                 await asyncio.sleep(0.1)
-
-        except Exception as e:
-            print(f"Could not send metrics: {e}")
-            pass
+            except Exception as e:
+                print(f"Error in sending metrics: {e}")
+                break  # exit the loop if there is an error
 
 
 async def receive_connection(websocket, path):
     ws.append(websocket)
     try:
-        try:
-            async for message in websocket:
-                await websocket.wait_closed()
-        except Exception as e:
-            asyncio.run(send_metrics())
+        async for message in websocket:
+            pass
+    except Exception as e:
+        print(f"Error in WebSocket: {e}")
     finally:
         ws.remove(websocket)
